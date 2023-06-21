29a416f8f171d8ef715288031aeace19b264bace
mishuk09 <mishukinfo09@gmail.com>
2023-06-21 12:16:27 -0400

    dsa

---
+++ Array/binarySearch.java
@@ -0,0 +1,12 @@
+package Array;
+
+public class binarySearch {
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 4, 5, 5667, 778, 8 };
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+        System.out.println();
+    }
+
+}
