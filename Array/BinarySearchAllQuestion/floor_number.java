e07313f0da8d84b581f31c6d38130eadcacfec37
mishuk09 <mishukinfo09@gmail.com>
2023-06-22 14:50:50 -0400

    dsa

---
+++ Array/BinarySearchAllQuestion/floor_number.java
@@ -0,0 +1,30 @@
+package Array.BinarySearchAllQuestion;
+
+public class floor_number {
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 4, 5, 6, 7, 8, 9 };
+        int target = 3;
+        int result = Floor_number(arr, target);
+        System.out.println(result);
+
+    }
+
+    static int Floor_number(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (target < arr[mid]) {
+                end = mid - 1;
+            } else if (target > arr[mid]) {
+                start = mid + 1;
+            } else {
+                return mid;
+            }
+
+        }
+        return end;
+    }
+
+}
